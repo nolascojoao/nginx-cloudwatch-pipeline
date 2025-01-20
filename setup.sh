62fb261e3e16ee781c7c286e9429af0d2e08217e
João Pedro Nolasco <nolascojoaops@gmail.com>
2025-01-20 11:31:29 -0300

    fix: use absolute path for config.json in setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo systemctl start nginx
 sudo systemctl enable nginx
 
 # Configuração do CloudWatch Logs
-sudo mv ~/cicd/config.json /opt/aws/amazon-cloudwatch-agent/bin/config.json
+sudo mv /home/ec2-user/cicd/config.json /opt/aws/amazon-cloudwatch-agent/bin/config.json
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -a fetch-config \
     -m ec2 \
